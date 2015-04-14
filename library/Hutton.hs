de6d5fba52de9b69d3da40cafd1dff95b16f93e5
Taylor Fausak <taylor@fausak.me>
2015-04-13 21:54:05 -0500

    Listen to the button's web socket

--- library/Hutton.hs
+++ library/Hutton.hs
@@ -1,4 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Hutton where
 
+import Hutton.WebSockets (runSecureClient)
+
+import Control.Monad (forever, void)
+import Data.ByteString.Builder (toLazyByteString)
+import Data.ByteString.Char8 (pack)
+import Data.ByteString.Lazy.Char8 (unpack)
+import Data.Text (Text)
+import Flow
+import Network.HTTP.Types (encodePath)
+import Network.Socket (PortNumber)
+import Network.WebSockets (ClientApp, receiveData)
+import System.Environment (getArgs)
+
 main :: IO ()
-main = return ()
+main = do
+    [h, e] <- getArgs
+    runSecureClient host port (path h e) application
+
+application :: ClientApp ()
+application connection = forever .> void <| do
+    message <- receiveData connection
+    print (message :: Text)
+
+host :: String
+host = "wss.redditmedia.com"
+
+port :: PortNumber
+port = 443
+
+path :: String -> String -> String
+path h e = encodePath segments query |> toLazyByteString |> unpack where
+    segments =
+        [ "thebutton"
+        ]
+    query =
+        [ ("h", Just (pack h))
+        , ("e", Just (pack e))
+        ]
