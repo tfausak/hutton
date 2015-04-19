f587f08656dd9c70fbb386354ceb07cf8332b30e
Taylor Fausak <taylor@fausak.me>
2015-04-19 12:44:31 -0500

    Make pressing the button actually work

--- Main.hs
+++ Main.hs
@@ -15,8 +15,8 @@ import Data.Time.Clock (UTCTime)
 import Data.Time.Format (defaultTimeLocale, parseTimeOrError)
 import Flow ((|>), (<|))
 import Network.HTTP.Client (Manager, Request)
-import Network.HTTP.Conduit (httpLbs, parseUrl, responseBody, simpleHttp,
-    urlEncodedBody, withManager)
+import Network.HTTP.Conduit (httpLbs, parseUrl, requestHeaders, responseBody,
+    simpleHttp, urlEncodedBody, withManager)
 import Network.WebSockets (Connection, receiveData)
 import Rainbow (Chunk, Radiant, blue, cyan, fore, green, magenta, putChunk,
     red, yellow, (<>))
@@ -27,6 +27,7 @@ import Wuss (runSecureClient)
 
 import Prelude
 
+-- Get command-line arguments.
 main :: IO ()
 main = do
     args <- getArgs
@@ -36,6 +37,7 @@ main = do
             putStrLn "Usage: hutton THRESHOLD USERNAME PASSWORD"
             exitFailure
 
+-- Set up login request.
 main1 :: String -> String -> String -> IO ()
 main1 threshold' username password = do
     let threshold = read threshold' :: Int
@@ -50,6 +52,7 @@ main1 threshold' username password = do
         loginRequest = urlEncodedBody loginQuery initialLoginRequest
     withManager (main2 threshold loginRequest)
 
+-- Actually log in.
 main2 :: (MonadIO m) => Int -> Request -> Manager -> m ()
 main2 threshold loginRequest manager = do
     loginResponse <- httpLbs loginRequest manager
@@ -59,9 +62,11 @@ main2 threshold loginRequest manager = do
             putStrLn message
             exitFailure
 
+-- Get query parameters.
 main3 :: Int -> LoginResponse -> Manager -> IO ()
 main3 threshold login manager = do
     putStrLn "Logged in."
+    putStrLn ("    cookie = " ++ cookie login)
     putStrLn ("    modhash = " ++ modhash login)
 
     putStrLn "Getting query parameters..."
@@ -74,6 +79,7 @@ main3 threshold login manager = do
             putStrLn "Failed to get query parameters."
             exitFailure
 
+-- Connect to WebSocket.
 main4 :: String -> String -> Int -> LoginResponse -> Manager -> IO ()
 main4 h e threshold login manager = do
     putStrLn "Got query parameters."
@@ -86,12 +92,14 @@ main4 h e threshold login manager = do
         path = "/thebutton?h=" ++ h ++ "&e=" ++ e
     runSecureClient host port path (main5 threshold login manager)
 
+-- Set up WebSocket client.
 main5 :: Int -> LoginResponse -> Manager -> Connection -> IO ()
 main5 threshold login manager connection = do
     putStrLn "Connected to WebSocket."
     ref <- newIORef 0
     forever (main6 connection ref threshold login manager)
 
+-- Listen for messages.
 main6 :: Connection -> IORef Int -> Int -> LoginResponse -> Manager -> IO ()
 main6 connection ref threshold login manager = do
     value <- receiveData connection
@@ -99,6 +107,7 @@ main6 connection ref threshold login manager = do
         Right datum -> main7 datum ref threshold login manager
         Left message -> putStrLn message
 
+-- Figure out what to do with messages.
 main7 :: Datum -> IORef Int -> Int -> LoginResponse -> Manager -> IO ()
 main7 datum ref threshold login manager = do
     let thisNumPressers = numPressers datum
@@ -107,15 +116,19 @@ main7 datum ref threshold login manager = do
     prettyPrint datum
     when (secondsLeft datum <= threshold) (main9 datum login manager)
 
+-- Update the number of pressers.
 main8 :: IORef Int -> Int -> IO ()
 main8 ref thisNumPressers = do
         writeIORef ref thisNumPressers
         putStrLn ""
 
+-- Press the button!
 main9 :: Datum -> LoginResponse -> Manager -> IO ()
 main9 datum login manager = do
     putStrLn "Pressing the button..."
     initialPressRequest <- parseUrl "https://www.reddit.com/api/press_button"
+    -- All of this probably isn't necessary. I don't have enough throwaway
+    -- accounts to figure what the minimal set of parameters is.
     let pressQuery =
             [ ("prev_seconds", datum |> secondsLeft |> show |> fromString)
             , ("r", "thebutton")
@@ -125,7 +138,18 @@ main9 datum login manager = do
             , ("tick_time", datum |> datum_payload |> payload_nowStr |> fromString)
             , ("uh", login |> modhash |> fromString)
             ]
+        pressHeaders =
+            [ ("Accept", "application/json, text/javascript, */*; q=0.01")
+            , ("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+            , ("Cookie", "reddit_session=" <> (login |> cookie |> fromString))
+            , ("Origin", "http://www.reddit.com")
+            , ("Referer", "http://www.reddit.com/r/thebutton/")
+            , ("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36")
+            , ("X-Requested-With", "XMLHttpRequest")
+            ]
         pressRequest = urlEncodedBody pressQuery initialPressRequest
+            { requestHeaders = pressHeaders
+            }
     pressResponse <- httpLbs pressRequest manager
     print pressResponse
     putStrLn "Pressed the button."
@@ -150,13 +174,21 @@ instance FromJSON LoginJson where
     parseJSON _ = mzero
 
 data LoginData = LoginData
-    { login_modhash :: String
+    { login_cookie :: String
+    , login_modhash :: String
     } deriving (Show)
 
 instance FromJSON LoginData where
     parseJSON (Object o) = LoginData
-        <$> o .: "modhash"
+        <$> o .: "cookie"
+        <*> o .: "modhash"
     parseJSON _ = mzero
+
+cookie :: LoginResponse -> String
+cookie response = response
+    |> login_json
+    |> login_data
+    |> login_cookie
 
 modhash :: LoginResponse -> String
 modhash response = response
